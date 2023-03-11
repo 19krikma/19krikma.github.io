5e4c61aeeee1abbbbe212abaea65a7e50e1c1a26
Mark Krikunov <31541643+19krikma@users.noreply.github.com>
2023-03-11 13:36:14 +0000

    added php file login get for testing

---
+++ login_get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <form>
+    Welcome <?php echo $_GET["email"]; ?><br>
+    Your email address is: <?php echo $_GET["password"]; ?>
+    </form>
+
+    </body>
+</html>
